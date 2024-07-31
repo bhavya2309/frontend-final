2d13be3a672ea1754eea5b391a2475544f4258fd
bhavya2309 <111899704+bhavya2309@users.noreply.github.com>
2024-08-01 00:26:14 +0530

    Update firebase-config.js

--- src/shared/firebase/firebase-config.js
+++ src/shared/firebase/firebase-config.js
@@ -8,13 +8,13 @@ import { getAuth, signInWithPopup, GoogleAuthProvider } from "firebase/auth";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyCpoYZ3c8TBAZwBkX3i70CsdFTxO93LljA",
-  authDomain: "pizzaauth-9e220.firebaseapp.com",
-  projectId: "pizzaauth-9e220",
-  storageBucket: "pizzaauth-9e220.appspot.com",
-  messagingSenderId: "1055201624816",
-  appId: "1:1055201624816:web:f45161e3c5fa5acd173835",
-  measurementId: "G-BXT69TMSRN"
+  apiKey: "AIzaSyBANreDqES68bSHawGkUF-kGR3i0043ha4",
+  authDomain: "pizza-app-92b6e.firebaseapp.com",
+  projectId: "pizza-app-92b6e",
+  storageBucket: "pizza-app-92b6e.appspot.com",
+  messagingSenderId: "215816502179",
+  appId: "1:215816502179:web:c1967c44e4aa4d4059b816",
+  measurementId: "G-RQGMCY8D0K"
 };
 
 // Initialize Firebase
